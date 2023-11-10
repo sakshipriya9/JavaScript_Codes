80750ce88f4577cb35ca61c625d65d0910010343
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-10 23:44:11 +0530

    Variable_Demo

---
+++ dataTypeDemo.js
@@ -0,0 +1,23 @@
+var marks = 100; //number data type
+var name = "Sakshi"; //string data type
+var age; //undefined data type
+var company = null; //null data type
+var salary = undefined;
+var isStudent = false; //boolean data type
+console.log("Marks =", marks);
+console.log("Name =", name);
+console.log("Age =", age);
+console.log("Comapany =", company);
+console.log("Salary =", salary);
+console.log("Is a student?", isStudent);
+//object declaration
+var user = {name: "Sakshi", age: 22, course:"Fullstack Developer",company:"Google", isAStudent: true};
+console.log("Details of the user is:", user);
+var user1 ={
+    name: "Manshi",
+    age: 23,
+    course: "Fullstack Developer",
+    company: "Not Known",
+    isAStudent: true
+};
+console.log("another details of the user is:", user1);
