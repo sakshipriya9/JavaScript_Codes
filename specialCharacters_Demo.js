5531d85c14e31f212bfd860f392604c9b303a13f
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-11 00:02:11 +0530

    Special Characters Demo

---
+++ specialCharacters_Demo.js
@@ -0,0 +1,13 @@
+let text = "I am learning Javascript and want to be master in it";
+console.log(text);
+
+// \n this is for new line
+let text1 = "I am learning Javascript\n and want to be master in it";
+console.log(text1);
+
+// \t this is for tab space
+let text2 = "I am learning JavaScript\t and want to be master in it";
+console.log(text2);
+let name = "Sonali";
+let name1 = "Sakshi Priya";
+console.log(name, "\n", name1);
