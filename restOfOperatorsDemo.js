5fd5434e4a1f8efae6f6ec0e1b7e6f1663adacad
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-11 00:13:31 +0530

    Added Rest Of Operators Demo

---
+++ restOfOperatorsDemo.js
@@ -0,0 +1,22 @@
+//Relational Operators
+let x = 10;
+let y = 20;
+let z = 30;
+console.log("x < y", x < y);//less than
+console.log("x > y", x > y);//gretaer than
+console.log("y <= x", y <= x);//less than and equal to
+console.log("z >= x", z >= x);//greater than and equal to
+
+//Logical operators
+console.log(true && false);
+console.log(false || false);
+console.log(true && true);
+console.log(true ^ true);
+console.log(false ^ true);
+console.log((15 > 12) && (12 < 4));
+console.log(!false);//not false= true
+console.log(6 && 10);
+console.log(10 || 0);
+console.log(6 || 19);
+console.log(0 || -0);
+console.log(undefined/null);
