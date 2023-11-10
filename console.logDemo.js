0519fc2b6b17988724edbcf1bd46fbd1bb135679
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-10 22:29:22 +0530

    console.log demo

---
+++ console.logDemo.js
@@ -0,0 +1,14 @@
+//to print a integer(number)
+console.log(10);
+//To print some string
+console.log("Hello World");
+//decalre a variable and assign some value in it and print it
+var age = 39;
+console.log(age);
+//print string, integer and variable together
+console.log(34, "Random Text", age);
+//declare different variable in one line
+let a = 10, b = 20, c = 30;
+console.log(a, b, c);
+var name = "Sakshi", name1 = "Amit", name2 = "Jyoti";
+console.log(name, name1, name2);
