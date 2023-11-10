99d9421e4b9c33e8a69d5859988f2bb735476130
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-10 22:53:44 +0530

    Variable_Demo

---
+++ variable_Demo.js
@@ -0,0 +1,12 @@
+//to decalre var and print it
+var score = 456;
+var marks = 546;
+console.log(score,marks);
+//to declare let and print it
+let age = 24;
+let flag = 0;
+console.log(age, flag);
+//to decalre const and print
+const x = 111;
+const y = 121;
+console.log(x,y);
