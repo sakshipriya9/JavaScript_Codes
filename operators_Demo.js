d9909ac31d3a96917805ff05ed2cd6da50c15be7
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-10 23:49:59 +0530

    Arithmetic Operators and Assignment Operators Demo Codes

---
+++ operators_Demo.js
@@ -0,0 +1,25 @@
+//Arithmetic Operators
+let x = 45;
+let y = 5;
+console.log(x+y);//Addition
+console.log(x-y);//Subtraction
+console.log(x*y);//Multiplication
+console.log(x/y);//Divide
+console.log(x%y);//remainder
+console.log(y**3);//y^3
+console.log(x**2);//x^2
+
+//Assignment operators
+let a = 10;//here = just assigns value from RHS to LHS
+a += 7;// here += is actually doing a = a + 7;
+console.log("Value after addition is:", a);
+a -= 3;//a = a - 3;
+console.log("Value after subtraction is:", a);
+a *= 4;//a = a * 4;
+console.log("Value after multiplication is:", a);
+a /= 7;//a = a / 7;
+console.log("Value after division is: ", a);
+a %= 3; //a = a % 3;
+console.log("The remainder is: ", a);
+a **= 4;//a = a^4
+console.log("Value after exponent is:",a);
