f268138b130561969db25227c22900e4bb6a6d30
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-11 22:47:36 +0530

    if Elseif Else Demo Codes

---
+++ ifElseifElseDemo.js
@@ -0,0 +1,15 @@
+isUserPremium = false;
+isUserVIP = false;
+isUserSports = true;
+if(isUserPremium){
+    console.log("Enjoy the premium content on Hotstar");
+}
+else if(isUserVIP){
+    console.log("Enjoy the VIP content on Hotstar");
+}
+else if(isUserSports){
+    console.log("Enjoy the Sports content on Hotstar");
+}
+else{
+    console.log("Buy the subscription of anyone");
+}
