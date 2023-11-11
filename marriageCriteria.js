cc9be58e309f38bd8779b836af4b10c5acb76cfa
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-11 23:17:49 +0530

    Added marriage criteria age

---
+++ marriageCriteria.js
@@ -0,0 +1,9 @@
+let ageOfBoy = 20;
+let ageOfGirl = 20;
+if(ageOfBoy >= 21){
+    if(ageOfGirl >= 18){
+        console.log("Congratualtions! Youn both are eligible for marriage each other");
+    }else{
+        console.log("Wait till age will be greater than 18.");
+    }
+}console.log("You both will wait.");
