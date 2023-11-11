416d9f94b0f074b31f2c16c93640f0083156e11b
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-11 22:43:43 +0530

    If-Else Demo codes

---
+++ if-ElseDemo.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Hotstar Disney");
+let isHotstarPremium = true;
+if(isHotstarPremium){ // if this condition is true
+    console.log("Enjoy the content");// this block is printed
+}
+else{
+    console.log("Kindly get subscription of Hotstar Disney");// either this block is printed
+}
