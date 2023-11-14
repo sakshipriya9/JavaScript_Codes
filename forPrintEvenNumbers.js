f92d8e45e3a01206d657d47b8d8f8d3fb5d5c31d
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-15 00:02:12 +0530

    print Even_Numbers

---
+++ forPrintEvenNumbers.js
@@ -0,0 +1,6 @@
+//To print all Even Number till n natural number
+for(let i = 1; i <=20; i +=1){//for(let i = 2; i <= 20; )console.log(i) to print all even number
+    if(i % 2 ==0){//check if i is Even and if(i % 2 != 0) to check odd number
+        console.log(i);
+    }
+}
